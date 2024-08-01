a78a574f101afe4131464596234af3863742b155
Adrian <93000948+AdrianR3@users.noreply.github.com>
2024-08-01 16:38:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const width = 125;
 const height = 125;
 
-const version = 1;
-const size = 4 * version + 17;
+let version = 7;
+let size = 4 * version + 17;
 
 setDocDimensions(width, height);
 
@@ -10,19 +10,6 @@ setDocDimensions(width, height);
 const lines = [];
 
 // bt.setRandSeed(12345);
-
-// Testing star
-// const t = 6;
-// for (let i = 0; i < t; i++) {
-//   let x = [rect(30, 30, 45, 40)];
-//   bt.rotate(x, i*360/t)
-//   lines.push(x[0]);
-// }
-
-// let a = [rect(30, 30, 45, 40)];
-// let b = [rect(30, 40, 45, 40)];
-// drawLines(a);
-// drawLines(b);
 
 // Draw a rectangle at x, y of width w and height h
 function rect(w, h, x = 0, y = 0) {
@@ -39,12 +26,10 @@ function rect(w, h, x = 0, y = 0) {
 //function toBinaryArray(str) {
 //    return str.split('').map(char => char.charCodeAt(0).toStseparatorRing(2).padStart(8, '0')).join('');
 //}
-
 function generateQRCode(
   version = 1,
   matrix
 ) {
-    // version = 4;
 
     // Version 1 QR Code -> size = 21
     // Version 40 QR Code -> size = 177
@@ -82,33 +67,80 @@ function generateQRCode(
             setBlock(x + i, y + 8, v);
         }
     }
+    // Generate Alignment Patterns
+    function alignmentPattern(x, y) {
+      x-=2;
+      y-=2;
+        for (let i = 0; i <= 4; i++) {
+            for (let j = 0; j <= 4; j++) {
+                if (i == 0 || i == 4 || j == 0 || j == 4 || (i == 2 && j == 2)) {
+                    setBlock(x + i, y + j, 1);
+                } else {
+                    setBlock(x + i, y + j, 0);
+                }
+            }
+        }
+    }
 
+  
     // Finder patterns
+    // (Three big rings)
     drawFinderPattern(0, 0);
     drawFinderPattern(0, size - 7);
     drawFinderPattern(size - 7, 0);
 
-    // // Separator Rings around the finder patterns
+    // Separator Rings 
+    // (Dark rings around the finder patterns)
     separatorRing(-1, -1);
     separatorRing(-1, size-8);
     separatorRing(size-8, -1);
 
     // Alignment Patterns
     if (version > 1) {
+      const coords = getPossibleAlignmentCoords(version);
       // Add alignment patterns
-      
+      for (let cyi = 0; cyi < coords.length; cyi++) {
+        for (let cxi = 0; cxi < coords.length; cxi++) {
+          let x = coords[cxi];
+          let y = coords[cyi];
+          console.log(`${x}, ${y}, ${matrix[y][x]}`)
+          if (matrix[y][x] == 3) {
+            alignmentPattern(coords[cxi], coords[cyi])
+          }
+        }
+      }
     }
 
-    // Timing Patterns (alternating black and white stripes)
+    // Timing Patterns
+    // (Alternating black and white stripes)
     for (let i = 7; i < size - 7; i++) {
-        setBlock(i, 5, !(i % 2));
-    //     setBlock(i, 14, i % 2);
-    //     setBlock(6, i, i % 2);
-    //     setBlock(14, i, i % 2);
+        // Vertical Timing Pattern
+        setBlock(i, 6, !(i % 2));
+
+        // Horizontal Timing Pattern
+        setBlock(6, i, !(i % 2));
     }
 
     return matrix;
 }
+
+// Big Thanks to StackOverflow
+function getPossibleAlignmentCoords(version) {
+  if (version === 1) {
+    return [];
+  }
+  const intervals = Math.floor(version / 7) + 1;
+  const distance = 4 * version + 4; // between first and last alignment pattern
+  const step = Math.ceil(distance / intervals / 2) * 2; // To get the next even number
+  return [6].concat(Array.from(
+    { length: intervals },
+    (_, index) => distance + 6 - (intervals - 1 - index) * step)
+  );
+}
+
+console.log(`Version: ${version}`)
+console.log(getPossibleAlignmentCoords(version))
+
 function randomArray(size = 21) {
   const array = [];
     
@@ -120,7 +152,6 @@ function randomArray(size = 21) {
           } else {
             row.push(3);
           }
-            
         }
         array.push(row);
     }
@@ -128,18 +159,17 @@ function randomArray(size = 21) {
     return array;
 }
 
-// const finalQRArray = randomArray(size);
-const finalQRArray = generateQRCode(version, randomArray(size));
+let baseArray = randomArray(size);
+baseArray = Array.from({ length: size }, () => Array(size).fill(3));
+let finalQRArray = generateQRCode(version, baseArray);
 
 // Output QR code qrArray
-console.log(finalQRArray
-            .map(row => row.map(val => val == 0 ? ' ' : val == 3 ? '⋅' : '■')
-                            .join(' ')
-                )
+console.log(finalQRArray.map(row => row.map(val => val == 0 ? ' ' : val == 3 ? '⋅' : '■').join(' '))
             .join('\n')
            );
 
 // Convert QR Code Array into rectangles for drawing
+let renderEmpty = false;
 const s = height/finalQRArray.length;
 for(let y = 0; y < finalQRArray.length; y++) {
   for (let x = 0; x < finalQRArray[y].length; x++) {
@@ -148,10 +178,10 @@ for(let y = 0; y < finalQRArray.length; y++) {
       let a = [rect(s, s, s*x + s/2, height- s*y - s/2)];
       lines.push(a[0]);
     } else if (val == 3) {
-      let a = [rect(s/2, s/2, s*x + s/2, height- s*y - s/2)];
+      let a = [rect(s/3, s/3, s*x + s/2, height- s*y - s/2)];
       bt.rotate(a, 45)
       lines.push(a[0]);
-    } else {
+    } else if (renderEmpty) {
       let a = [rect(s/4, s/4, s*x + s/2, height- s*y - s/2)];
       lines.push(a[0]);
     }
